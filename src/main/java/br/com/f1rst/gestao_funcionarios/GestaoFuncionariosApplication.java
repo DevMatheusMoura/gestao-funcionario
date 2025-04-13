eea7bee0aa0d05d007e7105aa35f66bdcaad5def
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-04-13 03:45:00 -0300

    implementa api de teste

--- src/main/java/br/com/f1rst/gestao_funcionarios/GestaoFuncionariosApplication.java
+++ src/main/java/br/com/f1rst/gestao_funcionarios/GestaoFuncionariosApplication.java
@@ -2,9 +2,19 @@ package br.com.f1rst.gestao_funcionarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class GestaoFuncionariosApplication {
+
+	@GetMapping
+	public String getHomeTeste() {
+		return "Gestao Funcionarios - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoFuncionariosApplication.class, args);
