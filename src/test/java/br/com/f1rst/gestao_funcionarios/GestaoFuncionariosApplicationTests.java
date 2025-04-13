2a04db8f8f1fa95f3876a3854f97ed39db56824f
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-04-13 01:46:35 -0300

    inicia o projeto

---
+++ src/test/java/br/com/f1rst/gestao_funcionarios/GestaoFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.f1rst.gestao_funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
