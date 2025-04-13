b048120596004af73d461a53c9088d7288207ea3
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-04-13 06:12:53 -0300

    implementa contrato da api

---
+++ src/main/java/br/com/f1rst/gestao_funcionarios/funcionarios/application/api/FuncionariosAPI.java
@@ -0,0 +1,12 @@
+package br.com.f1rst.gestao_funcionarios.funcionarios.application.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/funcionarios")
+public interface FuncionariosAPI {
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    FuncionariosResponse postFuncionarios(FuncionariosRequest funcionariosRequest);
+}
---
+++ src/main/java/br/com/f1rst/gestao_funcionarios/funcionarios/application/api/FuncionariosRequest.java
@@ -0,0 +1,4 @@
+package br.com.f1rst.gestao_funcionarios.funcionarios.application.api;
+
+public class FuncionariosRequest {
+}
