c1119547424b7b45420ade62db57030bf58b2789
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-04-13 21:35:53 -0300

    implementa request e response

--- src/main/java/br/com/f1rst/gestao_funcionarios/funcionarios/application/api/FuncionariosRequest.java
+++ src/main/java/br/com/f1rst/gestao_funcionarios/funcionarios/application/api/FuncionariosRequest.java
@@ -1,5 +1,20 @@
 package br.com.f1rst.gestao_funcionarios.funcionarios.application.api;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
 public class FuncionariosRequest {
 
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    private String funcao;
+    @NotNull
+    private Double salario;
+    @NotBlank
+    private String telefone;
+    @NotBlank
+    private String endereço;
 }
--- src/main/java/br/com/f1rst/gestao_funcionarios/funcionarios/application/api/FuncionariosResponse.java
+++ src/main/java/br/com/f1rst/gestao_funcionarios/funcionarios/application/api/FuncionariosResponse.java
@@ -1,4 +1,10 @@
 package br.com.f1rst.gestao_funcionarios.funcionarios.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
 public class FuncionariosResponse {
+    private UUID idFuncionario;
 }
